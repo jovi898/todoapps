3921512ba9edc8521427aaf5205787d065d4c010
Javohir <jazmidinov@avo.uz>
2025-08-22 15:00:26 +0500

    create_todoapp

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'presentation/screens/home/view/home.screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}
